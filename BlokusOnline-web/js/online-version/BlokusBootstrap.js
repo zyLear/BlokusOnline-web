a7d0a704063b5024539d229dbbf2343ae998beb4
zylear <284449710@qq.com>
2018-07-24 00:46:52 +0800

    drag chess

--- BlokusOnline-web/js/online-version/BlokusBootstrap.js
+++ BlokusOnline-web/js/online-version/BlokusBootstrap.js
@@ -13,9 +13,16 @@ function clickChooseChess(chessName) {
     blokusUIController.clickChooseChess(chessName);
 }
 
+
+
 function lose() {
     blokusUIController.lose(blokusUIController.blokusController.currentColor);
 }
+
+// function mouseUp(event) {
+//
+//
+// }
 
 function chessDown($this) {
     var x = $this.id.split('x')[0];
@@ -26,4 +33,15 @@ function chessDown($this) {
 
 $(function () {
     blokusUIController = new BlokusUIController();
+    $(document).mouseup(function (event) {
+        blokusUIController.mouseUp(event.pageX, event.pageY);
+
+    });
+
+    $(document).mousemove(function (event) {
+        blokusUIController.moving(event.pageX, event.pageY);
+    });
+
+
+
 });
