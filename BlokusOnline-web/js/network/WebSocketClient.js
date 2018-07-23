a99e7a9968225bb81f08eb3235b4351aa9dff66f
xiezongyu <xiezongyu@changingedu.com>
2018-07-23 13:36:28 +0800

    websocket client

---
+++ BlokusOnline-web/js/network/WebSocketClient.js
@@ -0,0 +1,44 @@
+var ws;
+
+function sendMsg() {
+    ws.send(document.getElementById('inputText').value);
+}
+var wxId;
+
+function connect() {
+
+    wxId = document.getElementById('connectText').value
+    // 打开一个 web socket
+    ws = new WebSocket("ws://172.22.13.74:19090/ws?token=" + wxId);
+    // function connect() {
+
+
+
+
+    ws.onopen = function() {
+        // Web Socket 已连接上，使用 send() 方法发送数据
+        // ws.send("发送数据");
+        alert("连接成功");
+        console.log('connected');
+        setInterval("ping()", 30000);
+    };
+
+
+
+    ws.onmessage = function(evt) {
+        var received_msg = evt.data;
+        // alert("数据已接收...");
+        console.log("receive from server:" + received_msg);
+    };
+
+    ws.onclose = function() {
+        // 关闭 websocket
+        alert("连接已关闭...");
+    };
+
+}
+
+
+function ping() {
+    ws.send('{"wxId":"' + wxId + '","msgType":"ping"}')
+}
